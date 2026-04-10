ae2d81c9d0c1190bf9e2588485486a113eac8ccb
DeadSigma <Mr.zubochistka@gmail.com>
2026-04-10 20:48:21 +0100

    Исправил баг, когда игрок присваивает дополнитель искре нажатием по воздуху рядом с искрой, а так же баг, когда присваиваешь дополнитель искре глядя на ряд искр, то дополнитель присваивался дальним искрам вместо передней искры

--- BotaniaStory/ItemSparkAugment.cs
+++ BotaniaStory/ItemSparkAugment.cs
@@ -13,51 +13,75 @@ namespace BotaniaStory
 
             IWorldAccessor world = byEntity.World;
 
-            // Получаем направление взгляда игрока
             Vec3d eyePos = byEntity.Pos.XYZ.Add(0, byEntity.LocalEyePos.Y, 0);
             Vec3f viewVec = byEntity.Pos.GetViewVector();
             Vec3d lookDir = new Vec3d(viewVec.X, viewVec.Y, viewVec.Z);
 
             Entity[] nearbySparks = world.GetEntitiesAround(byEntity.Pos.XYZ, 5, 5, e => e is EntitySpark);
 
+            EntitySpark targetSpark = null;
+            double closestDistance = 5.0; // Максимальная дальность луча в 5 блоков
+
             foreach (Entity entity in nearbySparks)
             {
                 if (entity is EntitySpark spark)
                 {
-                    Vec3d dirToSpark = new Vec3d(spark.Pos.X - eyePos.X, spark.Pos.Y - eyePos.Y, spark.Pos.Z - eyePos.Z);
+                    // Целимся в визуальный центр искры
+                    Vec3d sparkCenter = new Vec3d(spark.Pos.X, spark.Pos.Y + 0.3, spark.Pos.Z);
 
-                    // Если смотрим на искру (смягчили до 0.90)
-                    if (dirToSpark.Length() < 5.0 && dirToSpark.Normalize().Dot(lookDir) > 0.90)
+                    // Вектор от глаз до центра искры
+                    Vec3d V = new Vec3d(sparkCenter.X - eyePos.X, sparkCenter.Y - eyePos.Y, sparkCenter.Z - eyePos.Z);
+
+                    // 1. Проекция вектора на направление взгляда (насколько искра "впереди" нас по лучу)
+                    double t = V.Dot(lookDir);
+
+                    // Искра должна быть спереди (t > 0) и ближе, чем предыдущая найденная цель
+                    if (t > 0 && t < closestDistance)
                     {
-                        // Проверяем, свободна ли она
-                        if (spark.WatchedAttributes.GetString("augment", "none") == "none")
+                        // 2. Вычисляем точку на луче, которая находится напротив искры
+                        Vec3d projection = new Vec3d(lookDir.X * t, lookDir.Y * t, lookDir.Z * t);
+
+                        // 3. Вычисляем кратчайшее расстояние от луча до центра искры
+                        Vec3d perpendicular = new Vec3d(V.X - projection.X, V.Y - projection.Y, V.Z - projection.Z);
+                        double distFromCenter = perpendicular.Length();
+
+                        // Если наш луч прошел не дальше, чем в 0.4 блоках от центра искры (создаем виртуальный хитбокс)
+                        if (distFromCenter < 0.4)
                         {
-                            string augmentType = this.Code.Path.Replace("sparkaugment-", "");
-
-                            spark.WatchedAttributes.SetString("augment", augmentType);
-                            spark.WatchedAttributes.MarkAllDirty();
-
-                            world.PlaySoundAt(new AssetLocation("game", "sounds/player/buildhigh"), spark.Pos.X, spark.Pos.Y, spark.Pos.Z, null, true, 16, 1f);
-
-                            if (world.Side == EnumAppSide.Client)
-                            {
-                                (world.Api as ICoreClientAPI)?.ShowChatMessage($"Дополнитель '{augmentType}' успешно установлен!");
-                            }
-
-                            if ((byEntity as EntityPlayer)?.Player.WorldData.CurrentGameMode != EnumGameMode.Creative)
-                            {
-                                slot.TakeOut(1);
-                                slot.MarkDirty();
-                            }
-
-                            handling = EnumHandHandling.Handled;
-                            return; // Выходим, дело сделано!
+                            closestDistance = t; // Обновляем рекорд близости
+                            targetSpark = spark;
                         }
                     }
                 }
             }
 
-            // Если не попали по пустой искре, отдаем управление игре
+            if (targetSpark != null)
+            {
+                if (targetSpark.WatchedAttributes.GetString("augment", "none") == "none")
+                {
+                    string augmentType = this.Code.Path.Replace("sparkaugment-", "");
+
+                    targetSpark.WatchedAttributes.SetString("augment", augmentType);
+                    targetSpark.WatchedAttributes.MarkAllDirty();
+
+                    world.PlaySoundAt(new AssetLocation("game", "sounds/player/buildhigh"), targetSpark.Pos.X, targetSpark.Pos.Y, targetSpark.Pos.Z, null, true, 16, 1f);
+
+                    if (world.Side == EnumAppSide.Client)
+                    {
+                        (world.Api as ICoreClientAPI)?.ShowChatMessage($"Дополнитель '{augmentType}' успешно установлен!");
+                    }
+
+                    if ((byEntity as EntityPlayer)?.Player.WorldData.CurrentGameMode != EnumGameMode.Creative)
+                    {
+                        slot.TakeOut(1);
+                        slot.MarkDirty();
+                    }
+
+                    handling = EnumHandHandling.Handled;
+                    return;
+                }
+            }
+
             base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
         }
     }
